8773e81806f046ab27d97876b5f627ab96533751
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-09 23:12:01 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NtLookbook::Application.config.secret_key_base = 'bd4e10585b26993137946f3731e57a198bd484b0103db1a047b1ae74763a84bfb37755a1544c36ce843f0de645846f35091a7aead0743843af516d4929b22476'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NtLookbook::Application.config.session_store :cookie_store, key: '_nt_lookbook_session'
