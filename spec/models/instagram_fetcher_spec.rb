78f110eca91a39252d1ef660eebb49549e6e91ee
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-11 14:40:10 -0600

    Add test to ensure no duplication of photos

--- spec/models/instagram_fetcher_spec.rb
+++ spec/models/instagram_fetcher_spec.rb
@@ -5,18 +5,7 @@ describe InstagramFetcher do
   subject { InstagramFetcher.new }
 
   describe 'fetching recent photos' do
-    describe 'when none are available' do
-      before do
-        allow(subject.client).to receive(:user_recent_media).and_return([])
-      end
-      
-      it 'does not create any new photos' do
-        subject.recent_photos
-        expect(Photo.count).to eq(0)
-      end
-    end
-
-    describe 'when photos are available' do
+    describe 'when new photo is available' do
       before do
         allow(subject.client).to receive(:user_recent_media).and_return(
           [ double('photo', images:
@@ -32,6 +21,26 @@ describe InstagramFetcher do
         expect(Photo.count).to eq(1)
       end
     end
-  end
 
+    describe 'when no new photos are available' do
+      before do
+        Photo.create!(
+          standard_url: 'http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_8.jpg',
+          thumbnail_url: 'http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_5.jpg'
+          )
+
+        allow(subject.client).to receive(:user_recent_media).and_return(
+          [ double('photo', images:
+            { 'standard_resolution' => {'url' => 'http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_8.jpg' },
+              'thumbnail' => { 'url' => 'http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_5.jpg' }
+            })])
+      end
+      
+      it 'does not create duplicate photos' do
+        expect(Photo.count).to eq(1)
+        subject.recent_photos
+        expect(Photo.count).to eq(1)
+      end
+    end
+  end
 end
