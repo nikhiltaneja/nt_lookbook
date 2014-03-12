e819c00c8f57f980d6ec751d1915f2d1e60251b7
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-12 00:48:22 -0600

    Add validations on photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,4 @@
 class Photo < ActiveRecord::Base
+  validates :standard_url, presence: true, uniqueness: true
+  validates :thumbnail_url, presence: true, uniqueness: true
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Photo do
   it "has a standard resolution url" do
-    photo = Photo.create!(
+    photo = Photo.create(
       standard_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_8.jpg",
       thumbnail_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_5.jpg"
       )
@@ -11,11 +11,42 @@ describe Photo do
   end
 
   it "has a thumbnail url" do
-    photo = Photo.create!(
+    photo = Photo.create(
       standard_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_8.jpg",
       thumbnail_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_5.jpg"
       )
 
     photo.thumbnail_url.should eq("http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_5.jpg")
+  end
+
+  it 'requires standard resolution url' do
+    photo = Photo.create(
+      thumbnail_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_5.jpg"
+      )
+
+    photo.should_not be_valid
+  end
+
+  it 'requires thumbnail url' do
+    photo = Photo.create(
+      standard_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_8.jpg"
+      )
+
+    photo.should_not be_valid
+  end
+
+  it 'requires unique urls' do
+    photo1 = Photo.create(
+      standard_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_8.jpg",
+      thumbnail_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_5.jpg"
+      )
+
+    photo2 = Photo.create(
+      standard_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_8.jpg",
+      thumbnail_url: "http://distilleryimage10.s3.amazonaws.com/288b6860a7c411e38b890e44eb87a70a_5.jpg"
+      )
+    
+    photo1.should be_valid
+    photo2.should_not be_valid
   end
 end
